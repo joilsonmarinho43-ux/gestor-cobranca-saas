216dc3b03d119f1d914b6081f1634bc6dc6726a3
joilsonmarinho43-ux <joilsonmarinho43@gmail.com>
2026-02-26 20:10:18 -0300

    Delete whatsapp directory

--- whatsapp/__init__.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def show():
-    st.header("📱 Módulo WhatsApp")
-    st.write("Configuração de mensagens e campanhas automáticas.")
-    
-    st.button("Parear WhatsApp (QR Code)")
-    st.text_area("Template da Mensagem")
